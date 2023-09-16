7e18d50f85cf4fe0fb27115a2185eceb2b5f6a57
sri <97357548+srivanik8@users.noreply.github.com>
2023-09-16 07:21:30 +0000

    updates

--- uc.js
+++ uc.js
@@ -1,6 +1,6 @@
 function getDiagnosis(questionId) {
-    // Get user's response for the specified question
-    var response = document.getElementById(questionId).value.toLowerCase();
+    // Get the selected response (yes or no)
+    var response = document.querySelector('input[name="' + questionId + '"]:checked');
     
     // Get the corresponding diagnosis paragraph element
     var diagnosisElement = document.getElementById(questionId + 'Diagnosis');
@@ -8,47 +8,51 @@ function getDiagnosis(questionId) {
     // Analyze the response and provide a detailed diagnosis
     var diagnosis = '';
 
+
+    if (response) {
+        response = response.value.toLowerCase();
+
     switch (questionId) {
         case 'menstrualCycle':
             if (response === 'yes') {
                 diagnosis = "Having regular menstrual cycles is a positive sign for your uterine health. Continue to track your cycles for any changes.";
             } else if (response === 'no') {
-                diagnosis = "Irregular menstrual cycles may indicate a hormonal imbalance or other uterine health issues. Consider tracking your cycles, maintaining a balanced diet, and consulting a healthcare professional for further evaluation.";
+                diagnosis = " Menstrual irregularities can be caused by factors like hormonal imbalances, stress, diet, and underlying medical conditions. Managing regular cycles may involve a balanced diet, stress reduction, and consulting a healthcare professional for guidance and treatment if needed.";
             }
             break;
         case 'pelvicPain':
             if (response === 'yes') {
-                diagnosis = "Experiencing pelvic pain or discomfort can be indicative of various uterine health problems. It's crucial to schedule an appointment with a healthcare professional for a thorough evaluation.";
+                diagnosis = "Pelvic pain or discomfort in a person can be caused by various factors like menstrual cramps, infections, or muscle strain. For pelvic pain, consult a doctor, use pain relievers, apply heat/cold packs, follow prescribed treatments, consider physical therapy, manage stress and stay hydrated.";
             } else if (response === 'no') {
                 diagnosis = "Not experiencing pelvic pain is a positive sign, but it's essential to remain vigilant about your uterine health. Regular check-ups with a healthcare provider are advisable.";
             }
             break;
         case 'vaginalBleeding':
             if (response === 'yes') {
-                diagnosis = "Experiencing vaginal bleeding between periods or after menopause can be concerning. It's essential to consult a healthcare professional for a thorough examination to identify the underlying cause.";
+                diagnosis = "Vaginal bleeding between periods or after menopause needs a doctor's evaluation. It can be due to hormonal changes, infections, growths, or cancer. Treatment varies based on the cause and may include medication, surgery, or other medical interventions. Seek prompt medical attention for proper care.";
             } else if (response === 'no') {
-                diagnosis = "Not experiencing vaginal bleeding between periods or after menopause is a positive sign. However, it's still important to stay vigilant about your uterine health and have regular check-ups.";
+                diagnosis = "The absence of vaginal bleeding between periods or post-menopause is encouraging. To safeguard uterine health, maintain regular check-ups with a healthcare professional as a proactive measure.";
             }
             break;
         case 'familyHistory':
             if (response === 'yes') {
-                diagnosis = "Having a family history of uterine conditions, such as fibroids or endometriosis, increases your risk. It's advisable to be proactive about your uterine health. Discuss your family history with a healthcare professional for guidance on preventive measures.";
+                diagnosis = "Having a family history of uterine conditions, such as fibroids or endometriosis, increases your risk. It's advisable to be proactive about your uterine health. consult a healthcare provider for a thorough evaluation. Causes may involve genetic factors. Care may include monitoring, medication, or surgical options based on diagnosis.";
             } else if (response === 'no') {
-                diagnosis = "Not having a family history of uterine conditions is a positive sign. However, it's still important to maintain regular check-ups to monitor your uterine health.";
+                diagnosis = "Absence of family history is reassuring. To maintain uterine health, prioritize regular check-ups, a balanced diet, exercise, stress management, and avoiding tobacco/alcohol. Stay informed about potential risks and symptoms.";
             }
             break;
         case 'pelvicInfection':
             if (response === 'yes') {
-                diagnosis = "A history of pelvic infection or sexually transmitted infection (STI) requires attention. Seek treatment and follow up with your healthcare provider to prevent potential complications.";
+                diagnosis = "A history of pelvic infection or sexually transmitted infection (STI) requires attention. consult a healthcare provider for diagnosis and treatment. Causes may include bacterial or viral agents. Maintain uterine health through regular screenings, safe sexual practices, vaccination (if applicable), and overall health maintenance.";
             } else if (response === 'no') {
-                diagnosis = "Not having a history of pelvic infection or STI is a positive sign. However, it's essential to maintain safe practices and regular check-ups for optimal uterine health.";
+                diagnosis = "While the absence of a pelvic infection or STI history is reassuring, prioritize uterine health with safe practices, regular check-ups, safe sex, vaccinations, and overall well-being to prevent future issues.";
             }
             break;
         case 'unusualOdors':
             if (response === 'yes') {
-                diagnosis = "Noticing unusual or strong odors during your periods can be a sign of infection or other issues. It's essential to consult a healthcare professional for an evaluation and appropriate treatment.";
+                diagnosis = "Noticing unusual or strong odors during your periods can be a sign of infection , bacterial imbalances, infections, or diet. Maintain uterine health with regular check-ups, good hygiene, a balanced diet, and consulting a healthcare provider for any concerning symptoms.";
             } else if (response === 'no') {
-                diagnosis = "Not experiencing unusual odors during your periods is a positive sign. However, it's still important to maintain good hygiene and regular check-ups for uterine health.";
+                diagnosis = "Absence of unusual period odors is reassuring. Maintain uterine health through good hygiene, regular check-ups, a balanced diet, staying hydrated, and promptly addressing any concerning symptoms for long-term well-being.";
             }
             break;
         case 'severePain':
@@ -60,28 +64,30 @@ function getDiagnosis(questionId) {
             break;
         case 'irregularBleeding':
             if (response === 'yes') {
-                diagnosis = "Experiencing irregular bleeding between your periods can be a sign of hormonal issues or other uterine health problems. It's crucial to consult a healthcare professional for evaluation and guidance.";
+                diagnosis = "If experiencing late periods, causes can include pregnancy, stress, hormonal imbalances, or underlying health issues. Maintain uterine health with regular check-ups, pregnancy tests if relevant, stress management, a balanced diet, and consult a healthcare provider for proper diagnosis and guidance.";
             } else if (response === 'no') {
-                diagnosis = "Not experiencing irregular bleeding between your periods is a positive sign. Continue to track your menstrual patterns for any changes and maintain regular check-ups.";
+                diagnosis = "The regularity of your periods is reassuring. To maintain consistency, prioritize stress management, a balanced diet, and regular check-ups for overall uterine health and peace of mind.";
             }
             break;
         case 'lowerAbdomenDiscomfort':
             if (response === 'yes') {
-                diagnosis = "Experiencing lower abdomen pain or discomfort outside of your menstrual cycle can have various causes. Consult with a healthcare professional for a thorough examination and personalized recommendations.";
+                diagnosis = "Experiencing lower abdomen pain or discomfort outside of your menstrual cycle causes can include ovarian cysts, endometriosis, or digestive issues. Maintain uterine health with regular check-ups, pain management, a healthy diet, exercise, and consult a healthcare provider for diagnosis and tailored care.";
             } else if (response === 'no') {
-                diagnosis = "Not experiencing lower abdomen discomfort outside of your menstrual cycle is a positive sign. However, it's essential to remain attentive to any changes in your abdominal health and consult a healthcare provider if needed.";
+                diagnosis = "Absence of abdominal discomfort between periods is reassuring. Stay vigilant for any changes, prioritize regular check-ups, maintain a balanced diet, exercise, and consult a healthcare provider promptly if needed for sustained well-being.";
             }
             break;
         case 'hormonalIssues':
             if (response === 'yes') {
-                diagnosis = "Having hormonal issues can impact your uterine health. It's crucial to consult with a healthcare professional to address and manage these issues. They can recommend appropriate treatments and interventions.";
+                diagnosis = "Having hormonal issues can impact your uterine health. potential causes include PCOS, thyroid problems, or menopause. Maintain uterine health with regular check-ups, hormone therapy if prescribed, a balanced diet, exercise, and stress management. Consult a healthcare provider for proper diagnosis and personalized care.";
             } else if (response === 'no') {
-                diagnosis = "Not having hormonal issues is a positive sign. However, it's important to stay vigilant about your overall health and have regular check-ups.";
+                diagnosis = "The absence of hormonal issues is reassuring. To maintain this, prioritize overall health through regular check-ups, a balanced lifestyle, stress management, and seek medical advice as needed for continued well-being.";
             }
             break;
         default:
             diagnosis = "Invalid question.";
             break;
+    }   } else {
+        diagnosis = "Please select a response.";
     }
 
     // Display the diagnosis in the corresponding paragraph element
